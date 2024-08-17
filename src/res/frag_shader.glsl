cb1e82743fcdde1cd91ea5264de46a47b6d9c383
Satwik <satwik292007@gmail.com>
2024-08-17 15:09:01 +0530

    Load and display image

--- src/res/frag_shader.glsl
+++ src/res/frag_shader.glsl
@@ -3,9 +3,24 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec2 TextCoord;
 
-void
-main()
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+/* uniform sampler2D ourTexture; */
+
+void main()
 {
-    FragColor = vec4 (ourColor, 1.0);
-};
+    FragColor = mix(texture(texture1, TextCoord), texture(texture2, TextCoord), 0.2);
+    /* FragColor = texture(ourTexture, TextCoord); */
+
+}
+
+/* out vec4 FragColor; */
+
+/* in vec4 vertexColor; */
+
+/* void main() */
+/* { */
+/*     FragColor = vertexColor; */
+/* } */
