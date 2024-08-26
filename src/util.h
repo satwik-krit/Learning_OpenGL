392399c55f470f7ae555edadfe734c6f6dddccc9
Satwik <satwik292007@gmail.com>
2024-08-26 12:12:57 +0530

    Use HandmadeMath for matrices and vector operations.

--- src/util.h
+++ src/util.h
@@ -1,13 +1,14 @@
 #ifndef UTIL_H
 #define UTIL_H
-
-struct Image
+#include <stdbool.h>
+// typedef enum {TRUE, FALSE} bool;
+typedef struct 
 {
     char* data;
     int width;
     int height;
     int colorChannels;
-};
+} Image;
 
 const char* LoadFile (const char* filePath);
 
@@ -20,5 +21,7 @@ void* LoadGLFunction (const char* name);
 int GetShaderCompileError (unsigned int shader, unsigned int shaderType);
 
 void WhatsTheProblemWindows ();
+
+bool _LoadImage (const char* imagePath, Image* image);
 
 #endif
